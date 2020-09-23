8a545d34f391da4efe23334b6135892380c68e33
Chris Andrew <chrisandrew119@gmail.com>
2020-09-23 17:58:39 +0530

    Syncing

--- Makefile
+++ Makefile
@@ -5,19 +5,26 @@ ifeq (question,$(firstword $(MAKECMDGOALS)))
   $(eval $(Q_ARGS):;@:)
 endif
 
-num_versions = 4
+num_versions = 6
 output = latex/
 roll_nums = smai_students2.csv
 timestamp = $$(date --iso-8601=seconds)
 
-question_file = questions/q14.tex
-assignment_id = 15
-start_time = 2020-09-23T09:00:15+05:30
-end_time = 2020-09-23T09:40:15+05:30
-shuffle_question = True
-shuffle_list = 0 1 2 3 4
+quiz = True
+part = 3
+sample = 0
+assignment = quiz
 
-all: create-build clean parse key images package backup
+question_file = quiz/1/p3.tex
+assignment_id = 1
+start_time = 2020-09-23T18:50:15+05:30
+end_time = 2020-09-23T19:05:15+05:30
+shuffle_question = True
+shuffle_options = True
+# shuffle_list = 0 1 2 3 4
+shuffle_list = -1
+
+all: create-build clean parse key images package
 
 create-build:
 	mkdir -p build
@@ -28,7 +35,7 @@ beamer:
 	done
 
 clean: clean-build clean-latex
-	rm -rf assignment.zip
+	rm -rf assignment.zip quiz.zip
 
 clean-build:
 	rm -f build/*.pdf build/*.out build/*.aux build/*.log build/*.toc build/*.nav build/*.snm
@@ -36,7 +43,7 @@ clean-build:
 
 clean-latex:
 	rm -rf latex/*.tex
-	rm -rf latex/assignment.json
+	rm -rf latex/*.json
 	rm -rf latex/*.csv
 
 images: beamer
@@ -47,26 +54,32 @@ images: beamer
   done
 
 key:
-	for f in latex/key*.tex; do \
+	for f in latex/*key*.tex; do \
 		xelatex -output-directory build/ $$f; \
 	done
 
 package:
-	zip -j assignment.zip build/images/*.png latex/assignment.json
+	zip -j "$(assignment).zip" build/images/*.png latex/assignment.json
 
 
 backup:
-	cp assignment.zip "archive/assignment_$(assignment_id)_$(timestamp).zip"
-	for f in build/key*.pdf; do \
+	cp "$(assignment).zip" "archive/$(assignment)_$(assignment_id)_$(timestamp).zip"
+	for f in build/*key*.pdf; do \
 		cp $$f "archive/keys/`basename $$f`_$(timestamp).pdf"; \
 	done
 
 parse:
 	python3 parser.py --num_versions $(num_versions) --question_file $(question_file) --output $(output) \
 	--assignment_id $(assignment_id) --start_time $(start_time) --end_time $(end_time) --roll_nums $(roll_nums) \
-	--shuffle_question $(shuffle_question) --shuffle_list $(shuffle_list)
+	--shuffle_question $(shuffle_question) --shuffle_list $(shuffle_list) --quiz $(quiz) --part $(part) --sample $(sample) \
+	--shuffle_options $(shuffle_options)
 
 sync: clean
 	git add .
 	git commit -m "Syncing"
 	git push origin master
+
+# 1 - 6:30- 6:45
+# 2 - 6:40 - 6:55
+# 3 - 6:50 - 7:05
+# 4 - 7:00 - 7:15
--- parser.py
+++ parser.py
@@ -7,6 +7,7 @@ import json
 import pandas as pd
 import numpy as np
 import sys
+import re
 
 
 class Assignment(object):
@@ -45,16 +46,19 @@ class Assignment(object):
 
             else:
                 if frame_flag and not enum_flag:
-                    q_obj.add_content(l)
+                    if "% Desc" in l:
+                        q_obj.desc = l.replace("% Desc", "")
+                    else:
+                        q_obj.add_content(l)
                 elif frame_flag and enum_flag:
                     if "\\item" in l:
                         q_obj.add_options(l, "% Ans" in l, "% None" in l)
                 else:
                     continue
 
-    def gen_key(self):
-        doc_name = "key-{}.tex".format(self.id_)
-        q_name = "q_key-{}.tex".format(self.id_)
+    def gen_key(self, quiz=False, part=0):
+        doc_name = "quiz-{}-part-{}-key.tex".format(self.id_, part) if quiz else "key-{}.tex".format(self.id_)
+        q_name = "q_k-{}.tex".format(self.id_)
         frames = []
 
         for q_num, q in enumerate(self.questions):
@@ -63,23 +67,36 @@ class Assignment(object):
             self._gen_main_doc(doc_name, q_name)
         print("Generated key of Assignment {}".format(self.id_))
 
-    def gen_versions(self, num_versions, shuffle_question=True, shuffle_list=None):
-        assignment = self._gen_json()
-        assigned_students = self.split_rolls(num_versions)
+    def gen_versions(self, num_versions, shuffle_question=True, shuffle_list=None, shuffle_options=True, quiz=False, part=1, sample=0):
+        assignment = self._gen_json(quiz)
+        assigned_students, _ = self.split_rolls(num_versions)
         for copy_id in range(num_versions):
             frames = []
             if shuffle_question:
+                if shuffle_list == [-1]:
+                    shuffle_list = list(range(len(self.questions)))
                 self._shuffle_questions(shuffle_list)
 
             doc_name = "main-{}-{}.tex".format(self.id_, copy_id)
             q_name = "q-{}-{}.tex".format(self.id_, copy_id)
 
-            for q_num, q in enumerate(self.questions):
-                q.randomize()
-                frames.append(q.pprint())
-                assignment["questions"].append(q.json(self.id_, q_num, copy_id,
-                                                      doc_name, self.start_time, self.end_time,
-                                                      assigned_students[copy_id]))
+            if sample <= 0:
+                for q_num, q in enumerate(self.questions):
+                    if shuffle_options:
+                        q.randomize()
+                    frames.append(q.pprint())
+                    assignment["questions"].append(q.json(self.id_, q_num, copy_id,
+                                                          doc_name, self.start_time, self.end_time,
+                                                          assigned_students[copy_id], quiz, part))
+            else:
+                assert sample * num_versions <= len(self.questions)
+                for q_num, q in enumerate(self.questions[copy_id*sample:(copy_id+1)*sample]):
+                    if shuffle_options:
+                        q.randomize()
+                    frames.append(q.pprint())
+                    assignment["questions"].append(q.json(self.id_, q_num, copy_id,
+                                                          doc_name, self.start_time, self.end_time,
+                                                          assigned_students[copy_id], quiz, part))
 
             self._gen_question_doc(q_name, frames)
             self._gen_main_doc(doc_name, q_name)
@@ -88,14 +105,62 @@ class Assignment(object):
             f.write(json.dumps(assignment, indent=4))
         print("Metadata added to %s" % os.path.join(self.output, "assignment.json"))
 
-    def _gen_json(self):
+    def gen_per_student(self, values, quiz, part):
+        assignment = self._gen_json(quiz)
+        assigned_students, r_nos = self.split_rolls(1)
+        for i, r_no in enumerate(r_nos):
+            frames = []
+            doc_name = "main-{}-{}.tex".format(self.id_, r_no)
+            q_name = "q-{}-{}.tex".format(self.id_, r_no)
+
+            v = self.gen_values(values)
+            for q_num, q in enumerate(self.questions):
+                frames.append(q.pprint(values=v[q_num]))
+
+                assignment["questions"].append(q.json(self.id_, q_num, r_no,
+                                                      doc_name, self.start_time, self.end_time,
+                                                      [assigned_students[0][i]], quiz, part))
+
+            self._gen_question_doc(q_name, frames)
+            self._gen_main_doc(doc_name, q_name)
+        print("Generated {} versions of Assignment {}".format(len(r_nos), self.id_))
+        with open(os.path.join(self.output, "assignment.json"), "w") as f:
+            f.write(json.dumps(assignment, indent=4))
+        print("Metadata added to %s" % os.path.join(self.output, "assignment.json"))
+
+    def gen_values(self, values):
+        v = []
+        for dic in values:
+            m = {}
+            used = []
+            for k in dic:
+                type_ = dic[k][0]
+                if type_ is int:
+                    x = random.randint(*dic[k][1])
+                    while x in used:
+                        x = random.randint(*dic[k][1])
+                    m[k] = x
+                    used.append(x)
+                elif type_ is float:
+                    x = random.uniform(*dic[k][1])
+                    while x in used:
+                        x = random.uniform(*dic[k][1])
+                    m[k] = x
+                    used.append(x)
+            v.append(m)
+
+        return v
+
+    def _gen_json(self, quiz):
         s = {
-            "title": "Test RQ",
-            "code": "trq{}".format(self.id_),
+            "title": "Quiz" if quiz else "Test RQ",
+            "code": "quiz{}".format(self.id_) if quiz else "trq{}".format(self.id_),
             "number": self.id_,
-            "description": "Bulk uploaded assignment for class review",
+            "description": "Quiz " if quiz else "Bulk uploaded assignment for class review",
             "questions": []
         }
+        if quiz:
+            s["quiz"] = True
         return s
 
     def _shuffle_questions(self, shuffle_list):
@@ -113,7 +178,10 @@ class Assignment(object):
     def split_rolls(self, num_versions):
         data = pd.read_csv(self.roll_nums)
         emails = [x for x in data["Email ID"]]
-        random.shuffle(emails)
+        r_nos = [x for x in data["Roll No."]]
+        rids = np.random.permutation(len(emails))
+        emails = [emails[i] for i in rids]
+        r_nos = [r_nos[i] for i in rids]
 
         idx = np.linspace(0, len(emails), num_versions+1)
 
@@ -122,7 +190,7 @@ class Assignment(object):
             assigned_students.append(emails[int(idx[copy_id]): int(idx[copy_id+1])])
         # pdb.set_trace()
         print("Split roll numbers into {} groups".format(num_versions))
-        return assigned_students
+        return assigned_students, r_nos
 
     def _gen_main_doc(self, doc_name, q_name):
         content = "\\documentclass[aspectratio=43]{beamer}\n" +\
@@ -147,6 +215,7 @@ class Question(object):
     def __init__(self):
         self.content = ""
         self.options = []
+        self.desc = ""
 
     def add_content(self, c):
         self.content += c + "\n"
@@ -160,7 +229,19 @@ class Question(object):
         random.shuffle(non_none_values)
         self.options = non_none_values + none_values
 
-    def pprint(self, key=False):
+    def fill_values(self, values):
+        ignore = ["[!htbp]"]
+        placeholders = set([x for x in re.findall(r"\[.*?\]", self.content) if x not in ignore])
+        output = self.content
+        for k in placeholders:
+            e_string = k.replace("[", "").replace("]", "")
+            for v in values:
+                e_string = e_string.replace(v, "%0.4f" % (values[v]) if type(values[v]) is float else str(values[v]))
+            e_val = eval(e_string)
+            output = output.replace(k, "%0.4f" % (e_val) if type(e_val) is float else str(e_val))
+        return output
+
+    def pprint(self, key=False, values=None):
         content = "\\begin{frame}[shrink=20]\n" +\
                   "\\section{}\n" +\
                   "%s \n" +\
@@ -169,15 +250,26 @@ class Question(object):
                   "\\end{enumerate}\n" +\
                   "\\end{frame}\n"
         options = [x.key_version() if key else str(x) for x in self.options]
-        content = content % (self.content, "\n".join(options))
+
+        if values:
+            output = self.fill_values(values)
+            options = [x.numeric_version(values) for x in self.options]
+            content = content % (output, "\n".join(options))
+        else:
+            content = content % (self.content, "\n".join(options))
         return content
 
-    def json(self, assign_id, q_num, copy_id, doc_name, start_time, end_time, assigned_students):
+    def json(self, assign_id, q_num, copy_id, doc_name, start_time, end_time, assigned_students, quiz=False, part=0):
+
+        title = "Quiz {}, Part {}, Question".format(assign_id, part) if quiz else "Class review {} Question".format(assign_id)
+        description = "Quiz {}, Part {}, Question {}".format(assign_id, part, q_num+1) if quiz else "Class review {} Question {}".format(assign_id, q_num+1)
+        description = description + "\n" + self.desc
+        code = "q_{}_{}_{}_{}".format(assign_id, part, copy_id, q_num) if quiz else "q_{}_{}_{}".format(assign_id, copy_id, q_num)
         s = {
-            "title": "Class review {}-{}".format(assign_id, q_num+1),
+            "title": title,
             "number": q_num + 1,
-            "code": "q_{}_{}_{}".format(assign_id, copy_id, q_num),
-            "description": "Class Review {}, Question {}".format(assign_id, q_num+1),
+            "code": code,
+            "description": description,
             "start_time": start_time,
             "end_time": end_time,
             "tas": [
@@ -220,21 +312,63 @@ class Option(object):
         else:
             return str(self)
 
+    def numeric_version(self, values):
+        ignore = ["[!htbp]"]
+        placeholders = set([x for x in re.findall(r"\[.*?\]", self.content) if x not in ignore])
+        output = self.content
+        for k in placeholders:
+            e_string = k.replace("[", "").replace("]", "")
+            for v in values:
+                e_string = e_string.replace(v, "%0.4f" % (values[v]) if type(values[v]) is float else str(values[v]))
+            e_val = eval(e_string)
+            output = output.replace(k, "%0.4f" % (e_val) if type(e_val) is float else str(e_val))
+        return output
+
 
 def make_assignment(args):
+    if args.shuffle_question not in ["True", "False"] or args.quiz not in ["True", "False"] or args.shuffle_options not in ["True", "False"]:
+        parser.print_help()
+        sys.exit(1)
+
+    shuffle_question = args.shuffle_question == "True"
+    shuffle_options = args.shuffle_options == "True"
+    quiz = args.quiz == "True"
+
     assignment = Assignment(args.question_file,
                             args.assignment_id,
                             args.output,
                             args.start_time,
                             args.end_time,
-                            args.roll_nums)
-    if args.shuffle_question not in ["True", "False"]:
-        parser.print_help()
-        sys.exit(1)
-    shuffle_question = args.shuffle_question == "True"
+                            args.roll_nums,
+                            )
 
-    assignment.gen_key()
-    assignment.gen_versions(args.num_versions, shuffle_question, args.shuffle_list)
+    assignment.gen_key(quiz=quiz, part=args.part)
+    # assignment.gen_versions(args.num_versions, shuffle_question, args.shuffle_list, shuffle_options, quiz=quiz, part=args.part, sample=args.sample)
+    values = [
+        {
+            "m": (int, [2, 6]),
+            "p": (float, [0.1, 0.9]),
+        },
+        {
+            "w_1": (int, [1, 10]),
+            "w_2": (int, [1, 10]),
+            "b_1": (int, [1, 10]),
+            "b_2": (int, [1, 10]),
+        },
+        {
+            "K": (int, [0, 10])
+        },
+        {
+            "A": (int, [0, 10]),
+            "B": (int, [0, 10]),
+            "C": (int, [0, 10]),
+            "D": (int, [0, 10]),
+        },
+        {
+            "N": (int, [3, 10])
+        }
+    ]
+    # assignment.gen_per_student(values, quiz, args.part)
     pass
 
 
@@ -244,12 +378,15 @@ if __name__ == "__main__":
     parser.add_argument('--question_file', default="questions/sample.tex", type=str, help='File containing the questions')
     parser.add_argument('--output', default="latex/", help="Path to output folder")
     parser.add_argument('--assignment_id', default=0, type=int, help="Assignment number")
+    parser.add_argument('--sample', default=0, type=int, help="Questions to sample per set")
+    parser.add_argument('--part', default=0, type=int, help="Part number of quiz")
     parser.add_argument('--start_time', type=str, help="Start time")
     parser.add_argument('--end_time', type=str, help="End time")
     parser.add_argument('--roll_nums', default="rolls.csv", type=str, help="CSV containing the roll number and emails of students")
     parser.add_argument('--shuffle_question', default="True", type=str, help="Shuffle question order, [True or False]")
+    parser.add_argument('--shuffle_options', default="True", type=str, help="Shuffle options order, [True or False]")
+    parser.add_argument('--quiz', default="False", type=str, help="If the assignment is a quiz, [True or False]")
     parser.add_argument('--shuffle_list', default=[], nargs='+', type=int,
                         help='List of questions that can be shuffled, if not given and --shuffle_question is True all will be shuffled')
     args = parser.parse_args()
-
     make_assignment(args)
